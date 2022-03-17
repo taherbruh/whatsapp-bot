4f94ffdc652cf98e9f874c2b701eb8fa3b8a2cd2
taherbruh <taher.hewait@student.giu-uni.de>
2022-03-17 19:02:10 +0200

    whatsapp bot

---
+++ whatsapp.py
@@ -0,0 +1,20 @@
+import pyautogui
+import webbrowser as wb
+import time
+wb.open("web.whatsapp.com")
+time.sleep(5)
+for i in range (20):
+    pyautogui.press("i")
+    pyautogui.press("l")
+    pyautogui.press("o")
+    pyautogui.press("v")
+    pyautogui.press("e")
+    pyautogui.press("y")
+    pyautogui.press("o")
+    pyautogui.press("u")
+    pyautogui.press("enter")
+
+
+
+
+
